627697faa7164e4adc609d7dcc9c8b166aa02ae2
marlon zayro <zayro8905@gmail.com>
2020-12-16 10:58:36 -0500

    agregar rutas a la lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,16 @@
 import 'package:flutter/material.dart';
 
+import './src/pages/contacts.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // lista de elementos
-  final _contacts = ["Miguel", "Marlon", "Edwin", "pepito"];
-
-  List<Widget> listado() {
-    return List.generate(
-        _contacts.length,
-        (i) => ListTile(
-            title:
-                Text("$i - ${_contacts[i]}", style: TextStyle(fontSize: 20.0)),
-            subtitle: Text("+573015862"),
-            leading: Icon(Icons.access_alarm_sharp),
-            trailing: Icon(Icons.access_alarm_sharp)));
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Material App Bar'),
-          ),
-          body: ListView(
-              // Ejemplo Lista Fija
-              /*  children: [
-              ListTile(
-                  title: Text("Marlon", style: TextStyle(fontSize: 20.0)),
-                  subtitle: Text("+573015862"),
-                  leading: Icon(Icons.access_alarm_sharp),
-                  trailing: Icon(Icons.access_alarm_sharp)),
-                 
-            ], */
-              children: listado())),
+      home: ContactPage(),
     );
   }
 }
---
+++ lib/src/pages/contacts.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import './info.dart';
+
+class ContactPage extends StatelessWidget {
+  // lista de elementos
+  final _contacts = ["Miguel", "Marlon", "Edwin", "Diego"];
+
+  List<Widget> _cargarLista1() {
+    return List.generate(
+        _contacts.length,
+        (i) => ListTile(
+            title:
+                Text("$i - ${_contacts[i]}", style: TextStyle(fontSize: 20.0)),
+            subtitle: Text("+573015862"),
+            leading: Icon(Icons.access_alarm_sharp),
+            trailing: Icon(Icons.access_alarm_sharp)));
+  }
+
+  List<Widget> _cargarLista2() {
+    return _contacts.map((item) {
+      return ListTile(
+        title: Text(item),
+        subtitle: Text("12345678"),
+        trailing: Icon(Icons.access_alarms_rounded),
+        leading: Icon(Icons.ac_unit),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _cargarLista3() {
+      return _contacts.map((item) {
+        return ListTile(
+          title: Text(item),
+          subtitle: Text("12345678"),
+          trailing: Icon(Icons.arrow_forward_ios_outlined),
+          leading: CircleAvatar(
+            child: Text(item.substring(0, 1).toUpperCase()),
+          ),
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => InfoPage()));
+          },
+        );
+      }).toList();
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
+          ),
+          body: ListView(
+              // Ejemplo Lista Fija
+              /*  children: [
+              ListTile(
+                  title: Text("Marlon", style: TextStyle(fontSize: 20.0)),
+                  subtitle: Text("+573015862"),
+                  leading: Icon(Icons.access_alarm_sharp),
+                  trailing: Icon(Icons.access_alarm_sharp)),
+                 
+            ], */
+              children: _cargarLista3())),
+    );
+  }
+}
