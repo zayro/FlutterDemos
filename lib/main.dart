edb8ff3c55f7f03c86d57780e90adc7ebbe2e072
marlon zayro <zayro8905@gmail.com>
2021-01-09 22:29:23 -0500

    agregando tema base

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final _contacts = ["Miguel", "Marlon", "Edwin"];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.light,
+        primaryColor: Colors.yellow[700],
+        accentColor: Colors.grey[400],
+
+        // Define the default font family.
+        //fontFamily: 'Georgia',
+      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: Text('Material App Bar'),
           ),
           body: ListView(
-              /*  children: [
+            children: [
               ListTile(
                   title: Text("Marlon", style: TextStyle(fontSize: 20.0)),
                   subtitle: Text("+573015862"),
                   leading: Icon(Icons.access_alarm_sharp),
                   trailing: Icon(Icons.access_alarm_sharp)),
-                 
-            ], */
-              children: List.generate(
-                  _contacts.length, (i) => Text('$i ${_contacts[i]}')))),
+            ],
+          )),
     );
   }
 }
